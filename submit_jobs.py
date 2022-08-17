b1085925cee7a439679e13c343665a309b90e45e
Aaron Dotter <aaron.dotter@gmail.com>
2022-08-17 08:39:52 -0400

    switching to GS98 with new solar calibration

--- submit_jobs.py
+++ submit_jobs.py
@@ -106,7 +106,7 @@ if __name__ == "__main__":
     #Run Aaron's code to get the abundances
     shutil.copy(os.path.join(os.environ["XA_CALC_DIR"],"initial_xa_calculator"),os.environ['MIST_CODE_DIR'])
     os.system(os.path.join(os.environ['MIST_CODE_DIR'],"initial_xa_calculator") +\
-        " " +  net_name + " " + str(FeH) + " " +str(aFe) + " 1.3245")
+        " " +  net_name + " " + str(FeH) + " " +str(aFe) + " 1.3")
         
     #Zbase needs to be set in MESA for Type II opacity tables. Get this from a file produced by Aaron's code
     with open('input_XYZ') as f:
