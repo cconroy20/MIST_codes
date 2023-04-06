46a22b8e4a4b3b243662e415aae78d80054b6d32
Aaron Dotter <aaron.dotter@gmail.com>
2023-04-06 14:36:21 -0400

    more robust way to move the directory to long-term storage

--- reduce_jobs.py
+++ reduce_jobs.py
@@ -101,4 +101,4 @@ if __name__ == "__main__":
     print "************************************************************"
     print "****************MOVING TO LONG-TERM STORAGE*****************"
     print "************************************************************"
-    copytree(src=os.path.join(os.environ['MIST_GRID_DIR'],runname), dst=os.environ['STORE_DIR'], dirs_exist_ok=True)
+    copytree(src=os.path.join(os.environ['MIST_GRID_DIR'],runname), dst=os.path.join(os.environ['STORE_DIR'],runname), dirs_exist_ok=True)
